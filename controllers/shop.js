d180b7edae585117cc2d67abb795bfd1280eea4b
vgnsh1999 <vigneshvarathan@gmail.com>
2023-07-05 15:57:31 +0530

    Dynamic Routes Part 3 (Query Params)

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,16 +5,20 @@ exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
-      pageTitle: 'All Products',  
+      pageTitle: 'All Products',
       path: '/products'
     });
   });
 };
 
-exports.getProduct = (req,res,next) => {
-  const proId = req.params.productId;
-  Product.findById(proId, product =>{
-    res.render('/shop/product-detail',{product:product,pageTitle:product.title,path:'/products'});
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
   });
 };
 
@@ -35,10 +39,10 @@ exports.getCart = (req, res, next) => {
   });
 };
 
-exports.postCart = (req,res,next) => {
-  const prodID = req.body.productId;
-  Product.findById(prodID, (product) =>{
-    Cart.addProduct(prodID, product.price);
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
 };
