05a330e92fa053a1ac902462437c6193340e9367
vgnsh1999 <vigneshvarathan@gmail.com>
2023-07-06 13:09:21 +0530

    Create,Read and Delete operations using DB

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,23 +13,27 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
+  Product.findById(prodId)
+         .then(([product]) => {
+          res.render('shop/product-detail', {
+            product: product[0],
+            pageTitle: product.title,
+            path: '/products'
+          });
+         })
+         .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  });
+  Product.fetchAll()
+         .then(([rows, fieldData]) => {
+          res.render('shop/index', {
+            prods: rows,
+            pageTitle: 'Shop',
+            path: '/'
+          });
+         })
+         .catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
